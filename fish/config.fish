74f7c6f392a6645e5591ed90c790c75177cd6dcc
Kurt Preston <github@kurtpreston.com>
2025-07-30 11:02:18 -0700

    Adding per-shell reload command

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,11 @@ if test -f ~/.dotfiles/shared/aliases
     source ~/.dotfiles/shared/aliases
 end
 
+# Source fish-specific aliases
+if test -f ~/.dotfiles/fish/aliases
+    source ~/.dotfiles/fish/aliases
+end
+
 # Source local configuration if it exists
 if test -f ~/.localrc
     source ~/.localrc
