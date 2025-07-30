ddd8151df854edf033b151aa23e0ed68508ab2b1
Kurt Preston <github@kurtpreston.com>
2025-07-30 15:59:24 -0500

    Load ~/.localrc before tabcomplete for CODE_HOME override

--- fish/config.fish
+++ fish/config.fish
@@ -11,11 +11,6 @@ if test -f ~/.dotfiles/fish/aliases
     source ~/.dotfiles/fish/aliases
 end
 
-# Source tab completion functions
-if test -f ~/.dotfiles/fish/tabcomplete
-    source ~/.dotfiles/fish/tabcomplete
-end
-
 # Load NVM if available (Fish shell approach)
 if test -s ~/.nvm/nvm.sh
     # For Fish, we need to use bass to run nvm commands or use fisher nvm plugin
@@ -28,9 +23,14 @@ if test -s ~/.nvm/nvm.sh
     end
 end
 
-# Source local configuration if it exists
+# Source local configuration if it exists (before tab completion to allow CODE_HOME override)
 if test -f ~/.localrc
     source ~/.localrc
+end
+
+# Source tab completion functions (after local config to use any CODE_HOME override)
+if test -f ~/.dotfiles/fish/tabcomplete
+    source ~/.dotfiles/fish/tabcomplete
 end
 
 # Fish-specific configuration can be added here 
