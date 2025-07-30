67e35cb56c252efa73635e05a30236f0374615e0
Kurt Preston <github@kurtpreston.com>
2025-07-30 15:02:37 -0500

    Load nvm

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,18 @@ if test -f ~/.dotfiles/fish/aliases
     source ~/.dotfiles/fish/aliases
 end
 
+# Load NVM if available (Fish shell approach)
+if test -s ~/.nvm/nvm.sh
+    # For Fish, we need to use bass to run nvm commands or use fisher nvm plugin
+    # This sets up the NVM_DIR variable for compatibility
+    set -gx NVM_DIR ~/.nvm
+    
+    # Source nvm script using bass if available, otherwise skip
+    if command -v bass >/dev/null
+        bass source ~/.nvm/nvm.sh
+    end
+end
+
 # Source local configuration if it exists
 if test -f ~/.localrc
     source ~/.localrc
