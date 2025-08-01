d9d00e11809257085f9ea4dd849e88c72ffcc17c
Kurt Preston <github@kurtpreston.com>
2025-08-01 08:26:44 -0700

    Adding git_recursive_reset

--- fish/functions.fish
+++ fish/functions.fish
@@ -11,4 +11,21 @@ function c --description "Navigate to CODE_HOME or to a specific directory withi
         # With arguments: go to specific directory within CODE_HOME
         cd "$CODE_HOME/$argv[1]"
     end
-end 
+end
+
+# Aggressively reset project and submodules
+function git_recursive_reset --description "Aggressively reset project and submodules"
+    # Point all submodules to git commit
+    git submodule update --recursive --init
+
+    # Cancel any staged changes
+    git reset --hard
+    git submodule foreach --recursive git reset --hard
+
+    # Remove any lingering files or folders
+    git clean -xffd -e .env -e .vscode/settings.json
+    git submodule foreach --recursive git clean -xffd
+end
+
+# Alias for git_recursive_reset
+alias grr="git_recursive_reset" 
